5aff3f94ae9ddf07577f085161e13d555d988b18
ScottyBoy4 <38832386+ScottyBoy4@users.noreply.github.com>
2024-06-13 14:08:33 +1200

    Add files via upload

---
+++ HayNightGUI/HayNightGUI/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace HayNightGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.background = new System.Windows.Forms.PictureBox();
+            this.road = new System.Windows.Forms.PictureBox();
+            this.path2 = new System.Windows.Forms.PictureBox();
+            this.path1 = new System.Windows.Forms.PictureBox();
+            this.shipping = new System.Windows.Forms.PictureBox();
+            this.house = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.crops1 = new System.Windows.Forms.PictureBox();
+            this.crops2 = new System.Windows.Forms.PictureBox();
+            this.crops3 = new System.Windows.Forms.PictureBox();
+            this.crops4 = new System.Windows.Forms.PictureBox();
+            this.truck = new System.Windows.Forms.PictureBox();
+            this.deliveries = new System.Windows.Forms.PictureBox();
+            this.path3 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.car = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.background)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.road)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.path2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.path1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shipping)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.house)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.crops1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.crops2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.crops3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.crops4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.truck)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.deliveries)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.path3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // background
+            // 
+            this.background.BackColor = System.Drawing.Color.Green;
+            this.background.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("background.BackgroundImage")));
+            this.background.Location = new System.Drawing.Point(2, 1);
+            this.background.Name = "background";
+            this.background.Size = new System.Drawing.Size(798, 448);
+            this.background.TabIndex = 0;
+            this.background.TabStop = false;
+            this.background.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // road
+            // 
+            this.road.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.road.Image = ((System.Drawing.Image)(resources.GetObject("road.Image")));
+            this.road.Location = new System.Drawing.Point(2, 332);
+            this.road.Name = "road";
+            this.road.Size = new System.Drawing.Size(798, 89);
+            this.road.TabIndex = 1;
+            this.road.TabStop = false;
+            // 
+            // path2
+            // 
+            this.path2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.path2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("path2.BackgroundImage")));
+            this.path2.Location = new System.Drawing.Point(2, 54);
+            this.path2.Name = "path2";
+            this.path2.Size = new System.Drawing.Size(499, 76);
+            this.path2.TabIndex = 2;
+            this.path2.TabStop = false;
+            // 
+            // path1
+            // 
+            this.path1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.path1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("path1.BackgroundImage")));
+            this.path1.Location = new System.Drawing.Point(410, 116);
+            this.path1.Name = "path1";
+            this.path1.Size = new System.Drawing.Size(91, 220);
+            this.path1.TabIndex = 3;
+            this.path1.TabStop = false;
+            // 
+            // shipping
+            // 
+            this.shipping.BackColor = System.Drawing.Color.Blue;
+            this.shipping.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("shipping.BackgroundImage")));
+            this.shipping.Location = new System.Drawing.Point(508, 255);
+            this.shipping.Name = "shipping";
+            this.shipping.Size = new System.Drawing.Size(123, 81);
+            this.shipping.TabIndex = 4;
+            this.shipping.TabStop = false;
+            // 
+            // house
+            // 
+            this.house.BackColor = System.Drawing.Color.Green;
+            this.house.Image = ((System.Drawing.Image)(resources.GetObject("house.Image")));
+            this.house.Location = new System.Drawing.Point(498, 149);
+            this.house.Name = "house";
+            this.house.Size = new System.Drawing.Size(100, 69);
+            this.house.TabIndex = 5;
+            this.house.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.pictureBox7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox7.BackgroundImage")));
+            this.pictureBox7.Location = new System.Drawing.Point(613, 54);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(147, 164);
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackColor = System.Drawing.Color.Green;
+            this.pictureBox8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox8.BackgroundImage")));
+            this.pictureBox8.Location = new System.Drawing.Point(498, 60);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(100, 70);
+            this.pictureBox8.TabIndex = 7;
+            this.pictureBox8.TabStop = false;
+            // 
+            // crops1
+            // 
+            this.crops1.BackColor = System.Drawing.Color.Olive;
+            this.crops1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("crops1.BackgroundImage")));
+            this.crops1.Location = new System.Drawing.Point(304, 137);
+            this.crops1.Name = "crops1";
+            this.crops1.Size = new System.Drawing.Size(100, 190);
+            this.crops1.TabIndex = 8;
+            this.crops1.TabStop = false;
+            // 
+            // crops2
+            // 
+            this.crops2.BackColor = System.Drawing.Color.Olive;
+            this.crops2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("crops2.BackgroundImage")));
+            this.crops2.Location = new System.Drawing.Point(198, 136);
+            this.crops2.Name = "crops2";
+            this.crops2.Size = new System.Drawing.Size(100, 190);
+            this.crops2.TabIndex = 9;
+            this.crops2.TabStop = false;
+            // 
+            // crops3
+            // 
+            this.crops3.BackColor = System.Drawing.Color.Olive;
+            this.crops3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("crops3.BackgroundImage")));
+            this.crops3.Location = new System.Drawing.Point(92, 136);
+            this.crops3.Name = "crops3";
+            this.crops3.Size = new System.Drawing.Size(100, 190);
+            this.crops3.TabIndex = 10;
+            this.crops3.TabStop = false;
+            // 
+            // crops4
+            // 
+            this.crops4.BackColor = System.Drawing.Color.Olive;
+            this.crops4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("crops4.BackgroundImage")));
+            this.crops4.Location = new System.Drawing.Point(-14, 136);
+            this.crops4.Name = "crops4";
+            this.crops4.Size = new System.Drawing.Size(100, 190);
+            this.crops4.TabIndex = 11;
+            this.crops4.TabStop = false;
+            // 
+            // truck
+            // 
+            this.truck.BackColor = System.Drawing.Color.Gray;
+            this.truck.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("truck.BackgroundImage")));
+            this.truck.Location = new System.Drawing.Point(533, 336);
+            this.truck.Name = "truck";
+            this.truck.Size = new System.Drawing.Size(97, 39);
+            this.truck.TabIndex = 12;
+            this.truck.TabStop = false;
+            this.truck.Click += new System.EventHandler(this.truck_Click);
+            // 
+            // deliveries
+            // 
+            this.deliveries.BackColor = System.Drawing.Color.Blue;
+            this.deliveries.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("deliveries.BackgroundImage")));
+            this.deliveries.Location = new System.Drawing.Point(637, 255);
+            this.deliveries.Name = "deliveries";
+            this.deliveries.Size = new System.Drawing.Size(123, 81);
+            this.deliveries.TabIndex = 13;
+            this.deliveries.TabStop = false;
+            // 
+            // path3
+            // 
+            this.path3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.path3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("path3.BackgroundImage")));
+            this.path3.Location = new System.Drawing.Point(410, 221);
+            this.path3.Name = "path3";
+            this.path3.Size = new System.Drawing.Size(350, 31);
+            this.path3.TabIndex = 14;
+            this.path3.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(255, 417);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(498, 417);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 16;
+            this.pictureBox2.TabStop = false;
+            // 
+            // car
+            // 
+            this.car.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.car.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("car.BackgroundImage")));
+            this.car.Location = new System.Drawing.Point(361, 378);
+            this.car.Name = "car";
+            this.car.Size = new System.Drawing.Size(80, 40);
+            this.car.TabIndex = 17;
+            this.car.TabStop = false;
+            this.car.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 6;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.car);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.path3);
+            this.Controls.Add(this.deliveries);
+            this.Controls.Add(this.truck);
+            this.Controls.Add(this.crops4);
+            this.Controls.Add(this.crops3);
+            this.Controls.Add(this.crops2);
+            this.Controls.Add(this.crops1);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.house);
+            this.Controls.Add(this.shipping);
+            this.Controls.Add(this.path2);
+            this.Controls.Add(this.road);
+            this.Controls.Add(this.path1);
+            this.Controls.Add(this.background);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.background)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.road)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.path2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.path1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shipping)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.house)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.crops1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.crops2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.crops3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.crops4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.truck)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.deliveries)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.path3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox background;
+        private System.Windows.Forms.PictureBox road;
+        private System.Windows.Forms.PictureBox path2;
+        private System.Windows.Forms.PictureBox path1;
+        private System.Windows.Forms.PictureBox shipping;
+        private System.Windows.Forms.PictureBox house;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox crops1;
+        private System.Windows.Forms.PictureBox crops2;
+        private System.Windows.Forms.PictureBox crops3;
+        private System.Windows.Forms.PictureBox crops4;
+        private System.Windows.Forms.PictureBox truck;
+        private System.Windows.Forms.PictureBox deliveries;
+        private System.Windows.Forms.PictureBox path3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox car;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ HayNightGUI/HayNightGUI/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HayNightGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //Vars
+            int x = 0;
+            int y = 0;
+            Graphics paper = background.CreateGraphics();
+            Pen pen = new Pen(Color.Black, 2);
+            Brush brush = new SolidBrush(Color.DarkGreen);
+
+            //Clear Picturebox
+            background.Refresh();
+
+            //Draw a square
+            paper.FillRectangle(brush, x, y, x+798, y+448);
+            brush = new SolidBrush(Color.SandyBrown);
+            paper.FillRectangle(brush, x+(background.Width/2)-75, y, x + 100, y + 375);
+            brush = new SolidBrush(Color.Gray);
+            paper.FillRectangle(brush, x, (background.Width / 2) - 75, x + 798, y + 150);
+        }
+        int i = 0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (truck.Left == 663 || truck.Left == 533)
+            {
+                i++;
+                if(i == 300)
+                {
+                    truck.Left -= 1;
+                }
+            }
+            else
+            {
+                truck.Left -= 1;
+                i = 0;
+            }
+
+
+
+            if (truck.Left < -100)
+            {
+                truck.Left = 2000;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            timer2.Start();
+            
+        }
+
+        private void truck_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            car.Left -= rand.Next(1, 5);
+            if (car.Left < -100)
+            {
+                car.Left = rand.Next(1000,1500);
+            }
+        }
+    }
+}

06275c3f69fed713206e9497a1aa671a6c5e292d
ScottyBoy4 <38832386+ScottyBoy4@users.noreply.github.com>
2024-06-13 14:06:18 +1200

    Add files via upload

---
+++ HayDayGUI/HayDayGUI/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace HayDayGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.mainBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainBox
+            // 
+            this.mainBox.Location = new System.Drawing.Point(0, 0);
+            this.mainBox.Name = "mainBox";
+            this.mainBox.Size = new System.Drawing.Size(803, 450);
+            this.mainBox.TabIndex = 0;
+            this.mainBox.TabStop = false;
+            this.mainBox.Click += new System.EventHandler(this.mainBox_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mainBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.mainBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox mainBox;
+    }
+}
+
